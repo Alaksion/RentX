bfea426493c2bc91979397f5a7c4d9fd452853b2
Lucca Beurmann <luccab.souza@gmail.com>
2021-02-01 19:15:01 -0300

    login/register ui flow completed

--- app/src/main/java/com/example/rentx/authentication/presentation/registerforms/RegisterEmailFragment.kt
+++ app/src/main/java/com/example/rentx/authentication/presentation/registerforms/RegisterEmailFragment.kt
@@ -5,30 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.rentx.R
+import kotlinx.android.synthetic.main.fragment_register_email.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class RegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class RegisterFragment : Fragment(), View.OnClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,24 +20,28 @@ class RegisterFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_register_email, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterFragment()
-                .apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupListeners()
+        super.onViewCreated(view, savedInstanceState)
     }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            bt_register_mail_next.id ->
+                Navigation.findNavController(v)
+                    .navigate(R.id.action_registerFragment_to_registerPasswordFragment)
+
+            register_mail_go_back.id ->
+                Navigation.findNavController(v)
+                    .navigate(R.id.action_registerFragment_to_authMenuFragment)
+
+        }
+    }
+
+    private fun setupListeners() {
+        bt_register_mail_next.setOnClickListener(this)
+        register_mail_go_back.setOnClickListener(this)
+    }
+
+
 }
--- app/src/main/java/com/example/rentx/authentication/presentation/registerforms/RegisterPasswordFragment.kt
+++ app/src/main/java/com/example/rentx/authentication/presentation/registerforms/RegisterPasswordFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.rentx.R
+import kotlinx.android.synthetic.main.fragment_register_password.*
 
 
-class RegisterPasswordFragment : Fragment() {
+class RegisterPasswordFragment : Fragment(), View.OnClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,5 +18,27 @@ class RegisterPasswordFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register_password, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupListeners()
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            register_password_go_back.id ->
+                Navigation.findNavController(v)
+                    .navigate(R.id.action_registerPasswordFragment_to_registerFragment)
+
+            bt_submit_register.id ->
+                Navigation.findNavController(v)
+                    .navigate(R.id.action_registerPasswordFragment_to_registerSuccessFragment)
+        }
+    }
+
+    private fun setupListeners() {
+        register_password_go_back.setOnClickListener(this)
+        bt_submit_register.setOnClickListener(this)
     }
 }
